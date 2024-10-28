e1da630d657c87e9e1f6ab0d65b6616eab2663e8
Jayaprakash717 <prakashjaya717@gmail.com>
2024-10-28 23:19:02 +0530

    first commit

---
+++ Automate_OrangeHRM_JP/Admin/AdminData.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json.Linq;
+using Automate_OrangeHRM_JP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automate_OrangeHRM_JP
+{
+    public class AdminData : BasePage
+    {
+        public static string filename = "Admin.json";
+        public static JObject? AdminJ = ReadJson(@"JsonFile\AdminJ.json".ToString());
+        public static string Username = AdminJ.SelectToken("Uname").Value<string>();
+        public static string Pass = AdminJ.SelectToken("Password").Value<string>();
+        public static string Pasconfirm = AdminJ.SelectToken("PasswordConfirm").Value<string>();
+
+
+        public static JObject ReadJson(string filename)
+        {
+            string myJsonString = File.ReadAllText(@"..\\..\\..\\"+filename);
+            var myJObject = JObject.Parse(myJsonString);
+            return myJObject;
+        }
+    }
+}
---
+++ Automate_OrangeHRM_JP/AdminSearch/AdminSearchData.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using Automate_OrangeHRM_JP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automate_OrangeHRM_JP
+{
+    public class AdminSearchData : BasePage
+    {
+        public static string filename = "AdminSearchJ.json";
+        public static JObject? AdminSearchJ = ReadJson(@"JsonFile\AdminSearchJ.json".ToString());
+        public static string Username = AdminSearchJ.SelectToken("username").Value<string>(); 
+        public static string EditUsername = AdminSearchJ.SelectToken("editusername").Value<string>(); 
+
+        public static JObject ReadJson(string filename)
+        {
+            string myjsonstring = File.ReadAllText(@"..\\..\\..\\" + filename);
+            var myJObjct = JObject.Parse(myjsonstring);
+            return myJObjct;
+        }
+    }
+}
